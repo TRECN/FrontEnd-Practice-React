ec26b772a35701582c6f2c62c2908f0ac7f8064e
TRECN <rishabh1103@outlook.com>
2021-10-05 22:50:36 +0530

    commit

--- grid-challenge/src/App.js
+++ grid-challenge/src/App.js
@@ -9,7 +9,26 @@ function App() {
           <p> Gain access to our full library of tutorials along with expert code reviews. 
   Perfect for any developers who are serious about honing their skills.</p>
         </div>
-        
+        <div className="bottom">
+          <div className="left">
+            <p>Monthly Subscription</p>
+            <p> $29 per month</p>
+            <p>Full access for less than $1 a day</p>
+            <button className="signup">Sign Up</button>
+          </div>
+          <div className="right">
+            <p>Why Us</p>
+            <p>
+            Tutorials by industry experts <br />
+            Peer &amp; expert code review <br />
+            Coding exercises <br />
+            Access to our GitHub repos <br />
+            Community forum <br />
+            Flashcard decks <br />
+            New videos every week <br />
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
