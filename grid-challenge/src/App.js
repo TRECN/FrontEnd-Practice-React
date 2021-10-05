ddfdaec55393d3c189c1f8cc704f27ab75c7a213
TRECN <rishabh1103@outlook.com>
2021-10-05 22:45:50 +0530

    commit

--- grid-challenge/src/App.js
+++ grid-challenge/src/App.js
@@ -5,7 +5,11 @@ function App() {
       <div className="card">
         <div className="top">
           <h1>Join our community</h1>
+          <p>30-day, hassle-free money back guarantee</p>
+          <p> Gain access to our full library of tutorials along with expert code reviews. 
+  Perfect for any developers who are serious about honing their skills.</p>
         </div>
+        
       </div>
     </div>
   );
